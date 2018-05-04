9fc4aa473577d7cb951cc990e79c86dbc725e115
chunfortam <tom19921004@hotmail.com>
2018-05-03 17:01:03 -0700

    rotatString

--- src/Algothrim.java
+++ src/Algothrim.java
@@ -896,5 +896,51 @@ public class Algothrim {
         }
         return String.join(" ",words);
     }
+
+    /**
+     * We are given two strings A and B
+     * A shift on A consists of taking string A and moving the leftmost character to the rightmost position. For example
+     * if A = 'abcde', then it will be 'bcdea' after one shift on A. Return True if and only if A can become B after
+     * some numbers of shifts on A.
+     *
+     * Example 1:
+     * Input: A = 'abcde', B = 'cdeab'
+     * Output: True
+     *
+     * Example 1:
+     * Input A= 'abcde', B='cdeab'
+     * True
+     *
+     * Example 2:
+     * Input A = 'abcde', b = 'abcde'
+     * false
+     */
+
+    public boolean rotateString(String A, String B){
+
+        if(A.length() != B.length()){
+            return false;
+        }else{
+            int aPointer = 0;
+            char firstCharB = B.charAt(0);
+
+            for(int i = 0; i < A.length(); i ++){
+                if(A.charAt(i) == firstCharB){
+                    aPointer = i;
+                    break;
+                }
+            }
+
+            if(aPointer == 0){
+                return false;
+            }else{
+
+                String firstHalfRev = new StringBuilder(A.substring(0, aPointer)).reverse().toString();// String Reversed
+                String secondHalf = A.substring(aPointer,A.length());
+
+                return (secondHalf+firstHalfRev).equals(B);
+            }
+        }
+    }
 }
 
